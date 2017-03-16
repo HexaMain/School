37d701697a7282eb31f9dd78f41b630a91fdd616
Robin Marquardt <Robin.Marquardt@hexamain.de>
2017-03-16 10:29:39 +0100

    Class user created

---
+++ src/de/projectx/school/objects/User.java
@@ -0,0 +1,16 @@
+package de.projectx.school.objects;
+
+import java.util.UUID;
+
+public class User {
+	
+	public String firstname;
+	public String lastname;
+	public String email;
+	public UUID uuid;
+	
+	public User() {
+		// TODO Auto-generated constructor stub
+	}
+	
+}
